00f141f767906d62abe75e956ab1f502cb735498
Ronellyson <ronellysonjulio@gmail.com>
2021-09-07 03:19:44 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/ronellyson-j%C3%BAlio-nic%C3%A1ssio-de-oliveira-4481b61a2?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BbkTGalOMQQyn%2FeFpTd92Lw%3D%3D" target="_blank" rel="noreferrer">Ronellyson Júlio</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+          Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
